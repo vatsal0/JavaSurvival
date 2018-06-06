31636e736d56831265f3eb9ad09ad2d3b5cebf81
vatsal0 <0vatsal.b@gmail.com>
2018-06-06 15:27:53 -0400

    sal_tutorial1

--- main/js/main.js
+++ main/js/main.js
@@ -14,11 +14,26 @@ const base = createImage("../Assets/Homebase.png", function() {
     ctx.drawImage(base,0,canvas.height/10,resizeWidth(base.width),resizeHeight(base.height));
 });
 
-
 const troopButton = createImage("../Assets/TroopButton.png", function(){
     for (let i = 0; i < 800; i+= 200) {
         ctx.drawImage(troopButton, resizeWidth(550 + i), canvas.height-resizeHeight(200), resizeWidth(200), resizeHeight(200))
     }
+});
+
+const iconSoldier = createImage("../Assets/Homies/Soldier.png", function() {
+    ctx.drawImage(iconSoldier,resizeWidth(570.42),resizeHeight(979.76),resizeWidth(iconSoldier.width),resizeHeight(iconSoldier.height));
+});
+
+const iconSniper = createImage("../Assets/Homies/Sniper.png", function() {
+    ctx.drawImage(iconSniper,resizeWidth(770.22),resizeHeight(992.2),resizeWidth(iconSniper.width),resizeHeight(iconSniper.height));
+});
+
+const iconMinigunner = createImage("../Assets/Homies/Minigunner.png", function() {
+    ctx.drawImage(iconMinigunner,resizeWidth(969.8),resizeHeight(982.78),resizeWidth(iconMinigunner.width),resizeHeight(iconMinigunner.height));
+});
+
+const iconRocketLauncher = createImage("../Assets/Homies/Rocket Launcher.png", function() {
+    ctx.drawImage(iconRocketLauncher,resizeWidth(1169.53),resizeHeight(989.33),resizeWidth(iconRocketLauncher.width),resizeHeight(iconRocketLauncher.height));
 });
 
 function displayBase() {
